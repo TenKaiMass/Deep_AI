a8e84becbcf599d3fa7fc8bcad638fc588c6e283
TenKaiMass <mass.merouane@gamil.com>
2023-01-08 17:43:56 +0100

    Yolo configuration et training

--- MyModules/xmlGestion.py
+++ MyModules/xmlGestion.py
@@ -1,7 +1,7 @@
 import os
 import xml.etree.ElementTree as ET
 
-
+# fonction de calcul merci internet !
 def convert_box(size, box):
     dw, dh = 1. / size[0], 1. / size[1]
     x, y, w, h = (box[0] + box[1]) / 2.0 - 1, (box[2] + box[3]) / 2.0 - 1, box[1] - box[0], box[3] - box[2]
@@ -17,7 +17,7 @@ def xml_to_txt(path,image):
     if image.split('.')[1] == 'xml':
         # recupère le nom des images
         image_name = image.split('.')[0]
-        # Ajoute les version txt
+        # Ajoute les version txt vide
         return open(f'{path}{image_name}.txt', 'w')
         
 
@@ -25,8 +25,8 @@ def get_image_informations(path,image,labels):
     # conversion xml to txt
     file_gestor = xml_to_txt(path,image)
     
-    # On configure le curseur pour se balader dans le fichier
-    start = ET.parse(os.path.join('data','imagesxml', image))
+    # On configure le curseur pour se balader dans le fichier et recuperer les information
+    start = ET.parse(os.path.join('data','labels', image))
     curseur = start.getroot()       
     width = int(curseur.find('size') .find('width').text)
     height = int(curseur.find('size') .find('height').text)
@@ -34,8 +34,10 @@ def get_image_informations(path,image,labels):
         label = objet.find('name').text
         if label in labels and objet.find('difficult').text != '1':
             bndbox = objet.find('bndbox')
+            # Nous faisons des calculs sur la taille de l'item grace au position
             bb = convert_box((width, height), [float(bndbox.find(x).text) for x in ('xmin', 'xmax', 'ymin', 'ymax')])
             label_id = labels.index(label)  # class id
+            # ajout des infos dans un txt au meme nom
             file_gestor.write(" ".join([str(a) for a in (label_id, *bb)]) + '\n')
 
 
--- SignIA/Yolo5Sign.py
+++ SignIA/Yolo5Sign.py
@@ -4,16 +4,33 @@ import os
 from matplotlib import pyplot as plt
 import numpy as np
 import cv2
-import itertools
+from PIL import Image
 import torch
+import gradio as gr
+
+
+from typing import List
+
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import HTMLResponse, StreamingResponse
+
+import cv2
+import io
+import numpy as np
+
+import torch
+import cv2
+from PIL import Image
+
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
 
 path = './data/images'
 model = torch.hub.load('ultralytics/yolov5', 'custom', path='yolov5/runs/train/exp5/weights/best.pt', force_reload=True)
 fig, ax = plt.subplots(2,4, figsize=(20,10))
 imgs = os.listdir('./data/images')
+imgname = np.random.choice(imgs)
+img = cv2.imread(f'./data/images/{imgname}')
+results = model(img).show
 
-for idx in itertools.product(range(2),range(4)): 
-    imgname = np.random.choice(imgs)
-    img = cv2.imread(f'./data/images/{imgname}')
-    results = model(img)
-    ax[idx[0],idx[1]].imshow(cv2.cvtColor(np.squeeze(results.render()), cv2.COLOR_BGR2RGB))
